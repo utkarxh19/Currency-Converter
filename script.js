02bf76ece288ab9b5f9c1fa779bc58680e1aac64
utkarxh19 <114433073+utkarxh19@users.noreply.github.com>
2024-07-10 09:08:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL=""https://api.frankfurter.app/latest""
+const BASE_URL="https://api.frankfurter.app/latest"
 
 const dropdowns= document.querySelectorAll(".dropdown select");
 const btn=document.querySelector("form button");
